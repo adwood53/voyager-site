6c7dac0ee2c2e38ec9020c78c532d55e50d80a0a
AJ <a.j.woodward53@gmail.com>
2025-03-05 15:18:58 +0000

    reattempt

--- src/contexts/FirebaseContext.js
+++ src/contexts/FirebaseContext.js
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { createContext, useContext } from 'react';
 import { useUser, useOrganization } from '@clerk/nextjs';
 import { useFirebaseData } from './useFirebaseData';
