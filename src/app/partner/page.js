ff2de37e5999ad839852b282585521dc8c6c1060
AJ <a.j.woodward53@gmail.com>
2025-03-24 18:06:16 +0000

    ai bot 2

--- src/app/partner/page.js
+++ src/app/partner/page.js
@@ -2,11 +2,13 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { FirebaseProvider } from '@/src/contexts/FirebaseContext';
 import DashboardShell from '@/src/app/components/dashboard/DashboardShell';
 import MobileWarning from '@/src/app/components/dashboard/MobileWarning';
+import ChatbotComponent from '@/src/app/components/ChatbotComponent';
 
 export default function PartnerDashboard() {
   return (
     <ClerkProvider>
       <FirebaseProvider>
+        <ChatbotComponent />
         <MobileWarning />
         <DashboardShell />
       </FirebaseProvider>
